2b2e16903e9f7f37e89203a18e12211cd9c545a2
Dhavalbhai Maheshbhai Bakaraniya <d.bakaraniya@ostfalia.de>
2024-12-13 20:19:42 +0000

    Integrate MQTT to subscribe to a broker and update the latest moisture value

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+import paho.mqtt.client as mqtt
+import threading
 
 app = FastAPI()
 
 # Global variable to store the latest moisture value
 latest_moisture_value = "No Data Yet"
 
-@app.get("/humidity")
-def read_humidity():
-    return {"moisture_value": latest_moisture_value}
+# MQTT Setup
+BROKER = "test.mosquitto.org"
+TOPIC = "dar_val"
+
+def on_message(client, userdata, message):
+    global latest_moisture_value
+    latest_moisture_value = message.payload.decode()
+
+def mqtt_subscribe():
+    client = mqtt.Client()
+    client.on_message = on_message
+   
